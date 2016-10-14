0bf9e4cfda9a8491ab4596bf9dd0a78246be4c1f
highmember <ftn2539@gmail.com>
2016-10-14 21:31:44 +0700

    แก้ไขFuncLeftOperand ให้ครอบคลุมกรณีและเป็นไปตามโจทย์

--- spec/captcha.spec.js
+++ spec/captcha.spec.js
@@ -1,16 +1,13 @@
 function Captcha(pat,op,lop,rop){
-    var L = new LeftOperand(lop);
+    var L = new LeftOperand(pat,lop);
     var O = new Operator(op);
     var R = new RightOperand(rop);
   this.gen = function(){
-    if(pat === 1){
-      return R.toString() + O.toString() + L.toString();
-    }
-    else if(pat === 2){
-      return L.toString() + O.toString() + R.toString();
-    }
+    if(pat === 1)       {return L.toString() + O.toString() + R.toString();}
+    else if(pat === 2)  {return L.toString() + O.toString() + R.toString();}
   }
 }
+
 function Operator(ope){
   this.toString = function() {
     if(ope === 1){return ' + '}
@@ -19,17 +16,31 @@ function Operator(ope){
     else if(ope === 4){return ' / '}
   }
 }
-function LeftOperand(lope){
-  this.toString = function(){
+
+function LeftOperand(patt,lope){
+  l = ''
+  if(patt===2){
     switch (lope) {
-      case 1:return 'ONE';break;   case 2:return 'TWO';break;
-      case 3:return 'THREE';break; case 4:return 'FOUR';break;
-      case 5:return 'FIVE';break;  case 6:return 'SIX';break;
-      case 7:return 'SEVEN';break; case 8:return 'EIGHT';break;
-      case 9:return 'NINE';break;  case 0:return 'ZERO';break;
+      case 1:l += 'ONE';break;   case 2:l += 'TWO';break;
+      case 3:l += 'THREE';break; case 4:l += 'FOUR';break;
+      case 5:l += 'FIVE';break;  case 6:l += 'SIX';break;
+      case 7:l += 'SEVEN';break; case 8:l +=  'EIGHT';break;
+      case 9:l += 'NINE';break;  case 0:l +=  'ZERO';break;
     }
   }
+
+  else if(patt===1){
+    switch (lope){
+      case 0:l +=  '0'; break;     case 1:l +=  '1'; break;
+      case 2:l +=  '2'; break;     case 3:l +=  '3'; break;
+      case 4:l +=  '4'; break;     case 5:l +=  '5'; break;
+      case 6:l +=  '6'; break;     case 7:l +=  '7'; break;
+      case 8:l +=  '8'; break;     case 9:l +=  '9'; break;
+    }
+  }
+  this.toString = function(){return l}
 }
+
 function RightOperand(rope){
   this.toString = function(){
     switch (rope){
@@ -54,7 +65,10 @@ describe('Pattern 1', function () {
     var App = new Captcha(1,1,1,1)
     expect(App.gen()).toEqual('1 + ONE');
   });
-
+  it('should return "3 - FOUR" when input is 1,2,4,3', function () {
+    var App = new Captcha(1,2,4,3)
+    expect(App.gen()).toEqual('3 - FOUR');
+  });
 
 });
 });
