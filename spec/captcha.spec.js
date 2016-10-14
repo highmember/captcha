212455f7f3c44a4bdeda9d264ca3a3606c5ef720
highmember <ftn2539@gmail.com>
2016-10-14 20:14:25 +0700

    แก้ไข Leftoperand & operator

--- spec/captcha.spec.js
+++ spec/captcha.spec.js
@@ -1,44 +1,49 @@
 function Captcha(pat,op,lop,rop){
   this.gen = function(){
-    if(pat === 1)
-    {}
+    let Left = new LeftOperand(pat,lop)
+    let Op = new Operator(op)
+    let Right = new RightOperand(pat,rop)
+    if(pat === 1){
+      return Left.echo()+;
+    }
     else if(pat === 2)
     {}
   }
 }
 function Operator(op){
-
-    if(op === 1){return ' + '}
-    else if(op === 2){return ' - '}
-    else if(op === 3){return ' * '}
-    else if(op === 4){return ' / '}
+    var O = '';
+    if(op === 1){O += ' + '}
+    else if(op === 2){O += ' - '}
+    else if(op === 3){O += ' * '}
+    else if(op === 4){O += ' / '}
+    this.echo =function() {return O;}
 }
 function LeftOperand(pat,lop){
+  var L = '';
   if(pat===1){
     switch (lop) {
-      case 1: return '1';break;     case 2: return '2';break;
-      case 3: return '3';break;     case 4: return '4';break;
-      case 5: return '5';break;     case 6: return '6';break;
-      case 7: return '7';break;     case 8: return '8';break;
-      case 9: return '9';break;     case 0: return '0';break;
+      case 1: L+='1';break;     case 2: L+='2';break;
+      case 3: L+='3';break;     case 4: L+='4';break;
+      case 5: L+='5';break;     case 6: L+='6';break;
+      case 7: L+='7';break;     case 8: L+='8';break;
+      case 9: L+='9';break;     case 0: L+='0';break;
     }
   }
 
   else if(pat===2){
     switch (lop) {
-      case 1: return 'ONE';break;   case 2: return 'TWO';break;
-      case 3: return 'THREE';break; case 4: return 'FOUR';break;
-      case 5: return 'FIVE';break;  case 6: return 'SIX';break;
-      case 7: return 'SEVEN';break; case 8: return 'EIGHT';break;
-      case 9: return 'NINE';break;  case 0: return 'ZERO';break;
+      case 1: L+='ONE';break;   case 2: L+='TWO';break;
+      case 3: L+='THREE';break; case 4: L+='FOUR';break;
+      case 5: L+='FIVE';break;  case 6: L+='SIX';break;
+      case 7: L+='SEVEN';break; case 8: L+='EIGHT';break;
+      case 9: L+='NINE';break;  case 0: L+='ZERO';break;
     }
   }
+  this.echo =function() {return L;}
 }
 function RightOperand(pat,rop){
   if (pat=== 2){
-
     swich (rop){
-
       case 1: return '0'; break;
       case 2: return '1'; break;
       case 3: return '2'; break;
@@ -53,10 +58,8 @@ function RightOperand(pat,rop){
     }
   }
 
-  else if (pat=== 2){
-
+  else if (pat=== 1){
     swich (rop){
-
       case 1: return 'ZERO'; break;
       case 2: return 'ONE'; break;
       case 3: return 'TWO'; break;
@@ -70,17 +73,13 @@ function RightOperand(pat,rop){
 
     }
   }
-
-  this.rop = function(){
-
-  }
 }
 
 describe('Test Captcha App', function () {
 describe('Pattern 1', function () {
-  it('should return "1 + TWO" when input is 1,1,1,2', function () {
-    let App = new Captcha(1,1,1,2)
-    expect(App.gen).toEqual('1 + TWO');
+  it('should return "1 + TWO" when input is 1,1,1,1', function () {
+    let App = new Captcha(1,1,1,1)
+    expect(App.gen).toEqual('1 + ONE');
   });
 
 
